7d5b187e40ae42e40f6421ea012ad8be8d6b79b6
Denis <denis_hara@hotmail.com>
2023-01-18 17:42:01 -0300

    foi

--- Models/Suite.cs
+++ Models/Suite.cs
@@ -1,18 +1,29 @@
 namespace DesafioProjetoHospedagem.Models
 {
     public class Suite
+{
+    public Suite() { }
+    
+    public Suite(string tipoSuite, int capacidade, int valorDiaria)
     {
-        public Suite() { }
+        TipoSuite = tipoSuite;
+        Capacidade = capacidade;
+        ValorDiaria = valorDiaria;
+    }
 
-        public Suite(string tipoSuite, int capacidade, decimal valorDiaria)
-        {
-            TipoSuite = tipoSuite;
-            Capacidade = capacidade;
-            ValorDiaria = valorDiaria;
-        }
-
+        
         public string TipoSuite { get; set; }
         public int Capacidade { get; set; }
-        public decimal ValorDiaria { get; set; }
+        public int ValorDiaria { get; set; }
     }
-}
+
+}       
+
+
+
+
+
+
+
+
+    
